4938c69edaf948c2010803735262430f4c7fc26e
Mohd Shameem <istakeahmad34@gmail.com>
2026-03-02 01:47:05 +0530

    middleware

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
 app.listen(8080, () => {
-    console.log("Server is running on port 8080")
+  console.log("Server is running on port 8080");
 });
-app.get('/',(req,res)=>{
-    res.send("Api is working")
-})
+app.use(express.json())
 
+const auth = (req, res, next) => {
+  if (req.body.password === 123) {
+    next();
+  }
+  else{
+  return  res.json({
+        "Message":"Not Authorized"
+    })
+  }
+
+};
+app.use(auth);
+app.get("/", (req, res) => {
+  res.send("Api is working");
+});
