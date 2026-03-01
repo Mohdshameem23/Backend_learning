4be7f7efb362f3949e38ba654be41af3aeb3824b
Mohd Shameem <istakeahmad34@gmail.com>
2026-03-02 00:34:08 +0530

    2 March 2026

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const fs=require('fs');
-const data=fs.readFileSync('demo.pdf','utf-8');
-console.log(data); 
-console.log("Hello world")
+const express = require("express");
+const app = express();
+app.listen(8080, () => {
+    console.log("Server is running on port 8080")
+});
+app.get('/',(req,res)=>{
+    res.send("Api is working")
+})
+
